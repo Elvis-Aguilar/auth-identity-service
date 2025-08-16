c1537832cabb5031b0f78bf8e07bae34b64d11c9
Elvis Aguilar <elvisaguilar201930304@cunoc.edu.gt>
2025-08-15 21:56:21 -0600

    first commit

---
+++ src/main/java/com/eat/sleep/auth_identity_service/AuthIdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eat.sleep.auth_identity_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthIdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthIdentityServiceApplication.class, args);
+	}
+
+}
